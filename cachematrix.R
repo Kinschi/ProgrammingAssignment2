3540dd320e0f9af4a88628a5074344d1b8601e40
Stefan Kinschner <stefan.kinschner@fh-vie.ac.at>
2017-05-30 06:35:49 +0200

    New R Code for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions are about caching the inverse of a matrix (not having to compute
+## it repeatedly) which can be useful as matrix inversion is usually a costly
+## computation.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object and caches its inverse.
+## makeCacheMatrix contains the functions:
+  ## set (creating a matrix)
+  ## get (getting the matrix)
+  ## setinv (creating the inverse of the matrix)
+  ## getinv (getting the inverse of the matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  # Setting cached value to NULL at the beginning
+  inv <- NULL
+  
+  # Creating a matrix
+  set <- function(y) {
+      x <<- y
+      inv <<- NULL
+      }
+  
+  # Getting the matrix
+  get <- function () x
+  
+  # Creating the inverse of the matrix
+  setinv <- function(inverse) inv <<- inverse
+   
+  # Getting the inverse of the matrix
+  getinv <- function() inv
+ 
+  # returning list of functions
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
 }
 
+########################################
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of a matrix if not already cached
+## (in this case it it just returns the cached data) 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # getting the cached value      
+  inv <- x$getinv()
+  
+  # if inverse has already been calculated (is stored in cache): return the cached value 
+  if (!is.null(inv)) {
+    message ("getting cached data")
+    return(inv)
+    }
+  
+  # if not: computing and returning the inverse of the matrix
+  data <- x$get()
+  inv <- solve(data, ...)
+  
+  x$setinv(inv)
+  
+  inv
+  
 }
+
+
+
+
+
+
+
